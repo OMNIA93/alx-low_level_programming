3f1c33a51f295fdba4bc51303e17eb86f54dfe5e
omnia <omniannnnnnn@gmail.com>
2023-03-17 15:40:34 -0700

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+/**
+ * main - prints the last digit of arandomly generated number.
+ * and whether it is greater than 5,less than 6 ,or 0. 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ((n % 10) > 5)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}

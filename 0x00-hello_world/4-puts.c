fbb3bf740abb5784f9865694f8b326205916b3da
omnia <omniannnnnnn@gmail.com>
2023-03-16 19:30:27 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 /**
-* main - function to print by puts 
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

d09fe1a662ee20c11dce1158d035135b879ec715
omnia <omniannnnnnn@gmail.com>
2023-03-17 17:20:25 -0700

    print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ *  main - Entry point
+ *
+ *  Retur: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}

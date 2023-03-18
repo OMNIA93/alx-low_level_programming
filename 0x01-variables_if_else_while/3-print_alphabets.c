288776887d684a41af9c4215869609b400509adb
OMNIA93 <omniannnnnnn@gmail.com>
2023-03-18 00:18:26 +0000

    alphabets row

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - display the alphabet in a row
+ * Return: return 0 and exit the program
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}

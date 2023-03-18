309cc81157f232bc34c88e5ab68e810b7a4fa391
OMNIA93 <omniannnnnnn@gmail.com>
2023-03-18 00:22:57 +0000

    Display Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - display numbers from 0 to 9
+ * Return: return 0 and exit the program.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}

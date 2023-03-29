a767e9b90691cb423ed5573d112b5ef8f951591c
omnia <omniannnnnnn@gmail.com>
2023-03-29 16:34:37 -0700

    H

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * leet - encode into 1337speak
  * @n: input value
  * Return: n value
-*/
-
+ */
 char *leet(char *n)
 {
 int i, j;
@@ -13,12 +11,11 @@ char s1[] = "aAeEoOtTlL";
 char s2[] = "4433007711";
 for (i = 0; n[i] != '\0'; i++)
 {
-for (j = 0; j < 10; j++
+	for (j = 0; j < 10; j++)
 {
-if (n[i] == s1[j])
-{
-n[i] = s2[j];
-}
+		if (n[i] == s1[j])
+		n[i] = s2[j];
+	}
 }
 }
 return (n);

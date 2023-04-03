edca5189672de425be85301af13d0895e9e391ce
omnia <omniannnnnnn@gmail.com>
2023-04-03 07:28:32 -0700

    memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - files
+ * @s: pointer to put
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int l;
+
+	for (l = 0; n > l; l++)
+	{
+		s[l] = b;
+	}
+	return (s);
+}

8781cfc9c6e7fed50eed1b1b9d0d6794d1d0d247
omnia <omniannnnnnn@gmail.com>
2023-04-05 13:40:50 -0700

    prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,31 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * check - checks to see if number is prime
- * @a:int
- * @b:int
- * Return:int
+ * is_prime_number - check if n is a prime number
+ * @n: int
+ * Return: 0 or 1
  */
-int check(int a, int b)
+
+
+int is_prime_number(int n)
 {
-	if (b < 2 || b % a == 0)
-		return (0);
-	else if (a > b / 2)
-		return (1);
-	else
-		return (check(a + 1, b));
+	return (check_prime(n, 2));
 }
 
 /**
- * is_prime_number - states if number is prime
- * @n:int
- * Return:int
+ * check_prime - check all number < n if they can divide it
+ * @n: int
+ * @resp: int
+ * Return: int
  */
-int is_prime_number(int n)
+
+int check_prime(int n, int resp)
 {
-	if (n == 2)
+
+	if (resp >= n && n > 1)
 		return (1);
-	return (check(2, n));
+	else if (n % resp == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, resp + 1));
 }

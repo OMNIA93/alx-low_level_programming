6eb5294aa8cdcb3e9fd63c8bec904f4a0dc03cd2
omnia <omniannnnnnn@gmail.com>
2023-04-09 16:35:20 -0700

    n

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,15 +1,31 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * atoi - function that converts a string
- *       into an integer
- *
- * @s: string input pointer
- *
- * Return: 0 if there is no number
-*/
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
+ */
 
 int _atoi(char *s)
 {
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
+	}
+	res = sig * res;
+	return (res);
 }

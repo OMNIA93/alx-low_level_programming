846bd84546ee5ed59116563f94aa231e85314d56
omnia <omniannnnnnn@gmail.com>
2023-04-09 17:18:32 -0700

    mn

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# a script that creates a static library
+# called liball.a from all the .c files
+
+gcc -c *.c
+ar rc liball.a *.o
+echo y | rm -I *.o
--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -10,3 +10,4 @@ _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
 - Kevin Mitnick");
 return (0);
 }
+

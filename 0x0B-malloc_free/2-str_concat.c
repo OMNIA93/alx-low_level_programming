46a103aec04a8ef3766283714f39aed50b7e41af
omnia <omniannnnnnn@gmail.com>
2023-04-11 13:18:31 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,55 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* str_concat - function that concatenates two strings,
-* using dynamic memory allocation (malloc).
-*
-* @s1: a pointer to 1st string
-* @s2: a pointer to 2nd string
-*
-* Return:  a pointer to the string, or NULL if it fails.
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+
+/**
+* str_concat - Concat 2 strings.
+* @s1: string
+* @s2: string
+* Return: char
 */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *newstr, *temp;
-	unsigned int size, size1 = 0, size2 = 0, i = 0;
+	unsigned int l1, l2;
+	char *conc, *tmp;
 
 	if (!s1)
 		s1 = "";
 	else
-		while (*(s1 + size1++))
-			;
+		l1 = _strlen(s1);
 
 	if (!s2)
 		s2 = "";
 	else
-		while (*(s2 + size2++))
-			;
+		l2 = _strlen(s2);
 
-	size = size1 + size2 - 1;
-	newstr = malloc(size * sizeof(char));
+	conc = malloc(l1 + l2 + 1);
+	if (!conc)
+		return (0);
 
-	if (!newstr)
-		return (NULL);
-
-	temp = newstr;
+	tmp = conc;
 	while (*s1)
-		*temp++ = *s1++;
+		*tmp++ = *s1++;
 
-	while (*s2)
-		*temp++ = *s2++;
+	while ((*tmp++ = *s2++))
+		;
 
-return (newstr);
+	return (conc);
 }

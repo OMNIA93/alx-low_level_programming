b1ad161e0681efc2c9a71b7a410b4eb343663825
OMNIA93 <omniannnnnnn@gmail.com>
2023-05-01 17:29:28 -0700

    mnb

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,32 +1,23 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - a function that prints all elements of a list
- *
- * @h: pointer to first node
- *
- * Return: the number of nodes
-*/
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+	const listint_t *tp;
+	unsigned int cntr = 0;
 
-	/* return 0 as no of nodes when h is null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	tp = h;
+	while (tp)
 	{
-		printf("%d\n", h->n);
-
-		/*go to the next node*/
-		h = h->next;
-		/*count no of nodes*/
-		node_count++;
+		printf("%d\n", tp->n);
+		cntr++;
+		tp = tp->next;
 	}
-
-	/*print last node*/
-	printf("%d\n", h->n);
-
-	return (node_count);
+	return (cntr);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,8 +10,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
-*/
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 	int n;
@@ -28,3 +28,10 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif

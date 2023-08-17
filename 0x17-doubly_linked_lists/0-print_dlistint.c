eb6934e51a6236d80ea4ffa8e8e290a530a5ece1
omnia <omniannnnnnn@gmail.com>
2023-08-17 14:08:46 +0000

    print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a dlistint_t list.
+ * @h: A pointer to the head of the dlistint_t list.
+ * Return: The number of nodes in the list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t count = 0;
+    const dlistint_t *current = h;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+    }
+
+    return (count);
+}

946b99f3fd655e0fe1211df24458fb5b4e3391e2
omnia <omniannnnnnn@gmail.com>
2023-08-17 21:45:34 +0000

    get

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * @index: the index of the node, starting from 0
+ * @head: head node
+ * Return: the nth node or NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	while (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
+
+	if (head == NULL)
+		return (NULL);
+
+	return (head);
+}

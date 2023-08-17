1bf84d4dc88d588daff63688ddf04d921aa998e6
omnia <omniannnnnnn@gmail.com>
2023-08-17 23:08:07 +0000

    del

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index 
- * @head: points to the head of the list
+ * delete_dnodeint_at_index - deletes the node at index. 
+ * @head: points to the head of the list.
  * @index: the index of the node that should be deleted.
- * Return: 1 if it succeeded, -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed.
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {

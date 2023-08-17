3b2dbb6d581b609378f2dafd48b6c62c09cf3dd1
omnia <omniannnnnnn@gmail.com>
2023-08-17 14:33:34 +0000

    be

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    const dlistint_t *current = h;
+size_t count = 0;
+const dlistint_t *current = h;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+count++;
+}
 
-    return (count);
+return (count);
 }

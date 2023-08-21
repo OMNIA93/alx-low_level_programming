75b078f1855fb2f9ea703ce33c1ec810d1c1758d
omnia <omniannnnnnn@gmail.com>
2023-08-21 02:19:31 -0700

    is

---
+++ 0x18-dynamic_libraries/isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+_isupper - checks if a character is uppercase.
+@c: The character to be checked
+ Return: 1 if character is uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+    if (c >= "A" && c <= "Z")
+    Return (1);
+    else
+    Return (0);
+}

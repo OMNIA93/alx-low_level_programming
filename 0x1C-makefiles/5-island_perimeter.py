a44a95c8da9a1f5e1cf9ccd9b16296c68e348447
OMNIA93 <omniannnnnnn@gmail.com>
2023-10-25 17:21:19 -0700

    test

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that calculates the perimeter of an island in a grid."""
+
+def island_perimeter(grid):
+"""Returns the perimeter of the island in grid."""
+
+    if not grid or not grid[0]:
+        return 0
+    
+    perimeter = 0
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                   
